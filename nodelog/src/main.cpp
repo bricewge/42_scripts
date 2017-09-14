a5c41cbd2048335befa1462671af9078380c8b18
Brice Waegeneire <brice.wge@gmail.com>
2017-09-14 14:33:31 +0200

    avoid overflow in delay

--- nodelog/src/main.cpp
+++ nodelog/src/main.cpp
@@ -1,31 +1,35 @@
 #include "Arduino.h"
 #include "HID-Project.h"
 
-
-void setup() {
-  Keyboard.begin();
-  Consumer.begin();
+void setup()
+{
+	Keyboard.begin();
+	Consumer.begin();
 }
 
+void loop()
+{
+	unsigned long seconds = 1000L;
+	unsigned long minutes = seconds * 60;
 
-void loop() {
-  // Wait for the OS to ready
-  delay(1000);
-  // Set brightness to the minimum
-  for (int i = 0; i <= 16; i++)
-  	  Keyboard.write(KEY_F1);
-  // Lock the session
-  Keyboard.press(KEY_RIGHT_CTRL);
-  Keyboard.press(KEY_RIGHT_SHIFT);
-  Consumer.write(HID_CONSUMER_EJECT);
-  Keyboard.releaseAll();
-  // Wake up the computer regulary
-  while (42) {
-	delay(1000 * 60 * 30); // 30min
-    Keyboard.write(KEY_ESC);
-    delay(500);
-    Keyboard.write(KEY_ENTER);
-    delay(500);
-    Keyboard.write(KEY_ESC);
-  }
+	// Wait for the OS to ready
+	delay(1000);
+	// Set brightness to the minimum
+	for (int i = 0; i <= 16; i++)
+		Keyboard.write(KEY_F1);
+	// Lock the session
+	Keyboard.press(KEY_RIGHT_CTRL);
+	Keyboard.press(KEY_RIGHT_SHIFT);
+	Consumer.write(HID_CONSUMER_EJECT);
+	Keyboard.releaseAll();
+
+	// Wake up the computer regulary
+	while (42) {
+		delay(minutes * 30);
+		Keyboard.write(KEY_ESC);
+		delay(500);
+		Keyboard.write(KEY_ENTER);
+		delay(500);
+		Keyboard.write(KEY_ESC);
+	}
 }
