d59b59a2fca9f88364dcc2476cd8f26e97df596d
Brice Waegeneire <brice.wge@gmail.com>
2017-10-16 13:53:34 +0200

    nodelog: hide login and reorganize the code

--- nodelog/src/main.cpp
+++ nodelog/src/main.cpp
@@ -1,35 +1,58 @@
 #include "Arduino.h"
 #include "HID-Project.h"
 
+unsigned long second = 1000L;
+unsigned long minute = second * 60;
+
 void setup()
 {
 	Keyboard.begin();
 	Consumer.begin();
 }
 
-void loop()
+void lock_session()
 {
-	unsigned long seconds = 1000L;
-	unsigned long minutes = seconds * 60;
-
-	// Wait for the OS to ready
-	delay(1000);
-	// Set brightness to the minimum
-	for (int i = 0; i <= 16; i++)
-		Keyboard.write(KEY_F1);
-	// Lock the session
 	Keyboard.press(KEY_RIGHT_CTRL);
 	Keyboard.press(KEY_RIGHT_SHIFT);
 	Consumer.write(HID_CONSUMER_EJECT);
 	Keyboard.releaseAll();
+}
+
+void hide_login()
+{
+	Keyboard.press(KEY_RIGHT_ALT);
+	Keyboard.write(KEY_ENTER);
+	Keyboard.releaseAll();
+}
+
+void bright_min()
+{
+	for (int i = 0; i <= 16; i++)
+		Keyboard.write(KEY_F1);
+}
+
+void loop()
+{
+	// Wait for the OS to ready
+	delay(second);
+	lock_session();
+
+	delay(second * 2);
+	Keyboard.write(KEY_ESC); // Show lockscreen
+	delay(second / 2);
+	bright_min();
+	hide_login();
+	Keyboard.write(KEY_ESC); // Hide lockscreen
 
 	// Wake up the computer regulary
 	while (42) {
-		delay(minutes * 30);
+		delay(minute * 30);
 		Keyboard.write(KEY_ESC);
-		delay(500);
+		delay(second / 2);
+		Keyboard.press(KEY_TAB);
 		Keyboard.write(KEY_ENTER);
-		delay(500);
+		Keyboard.releaseAll();
+		delay(second / 2);
 		Keyboard.write(KEY_ESC);
 	}
 }
