307bb67e80bbcde787bd143c5881ea450f4624a9
Brice Waegeneire <brice.wge@gmail.com>
2017-10-18 12:00:22 +0200

    traveler: autodelog working
    It need a full rewriting thougth

--- traveler/autodelog.js
+++ traveler/autodelog.js
@@ -2,26 +2,53 @@ const got = require('got');
 const yaml = require('js-yaml');
 const fs = require('fs');
 const FormData = require('form-data');
+const os = require("os");
 
-
+const creds = yaml.safeLoad(fs.readFileSync('../credentials.yml', 'utf8'));
+const client_id = creds['client']['id'];
+const client_secret = creds['client']['secret'];
+const api_url = creds['url'];
 var token;
 
-try {
-    const creds = yaml.safeLoad(fs.readFileSync('../credentials.yml', 'utf8'));
-    const client_id = creds['client']['id'];
-    const client_secret = creds['client']['secret'];
-    const api_url = creds['url'];
+function get_acces_token(client_id, client_secret, api_url)
+{
 
     const form = new FormData();
     form.append('grant_type', 'client_credentials');
     form.append('client_id', client_id);
     form.append('client_secret', client_secret);
-    got.post(api_url + '/oauth/token', { body: form })
-        .then(response => { token = JSON.parse(response.body)['access_token'];
-                            console.log(token);
+    return got.post(api_url + '/oauth/token', { body: form })
+        .then(response => { return JSON.parse(response.body)['access_token'];
                           })
         .catch(error => { console.log(error); });
-} catch(e) {
-    console.log(e);
 }
 
+function get_user_location()
+{
+    const user = os.userInfo().username;
+    return get_acces_token(client_id, client_secret, api_url)
+        .then(token => {
+            return got(`${api_url}/v2/users/${user}/locations`,
+                { headers: { Authorization: `Bearer ${token}` } })
+                .then(response => {
+                   return JSON.parse(response.body)[0]['host'];
+                })
+                .catch(error => { console.log(error); });
+        })
+        .catch(error => { console.log(error); });
+}
+
+function get_current_host()
+{
+    return os.hostname().split('.')[0];
+}
+
+function is_location_saved()
+{
+    return get_user_location()
+        .then(location => {
+            process.exit( location === get_current_host() ? 0 : 1);
+        });
+}
+
+is_location_saved();
